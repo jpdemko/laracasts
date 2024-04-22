ca74adb04c22a8739f32c1c056151de03975c1e1
jpdemko <prestondemko@gmail.com>
2024-04-21 22:19:25 -0500

    Fin Epi 28

--- Validator.php
+++ Validator.php
@@ -8,4 +8,10 @@ class Validator
         $len = strlen($value);
         return $len >= $min and $len <= $max;
     }
+
+    public static function email($value)
+    {
+        $out = filter_var($value, FILTER_VALIDATE_EMAIL);
+        return $out;
+    }
 }
